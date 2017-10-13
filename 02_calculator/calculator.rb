b6b72b680fd4740fccb6dd02111bfea847e8dcc8
Anthony Bendas <anthony.bendas@gmail.com>
2017-10-12 19:51:07 -0600

    forked, raked 00-02 successfully

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,38 @@
 #write your code here
+def add (num1,num2)
+  num1+num2
+end
+
+def subtract (num1,num2)
+  num1-num2
+end
+
+def sum (array)
+  sum = 0
+  array.each do |i|
+    sum += i
+  end
+  return sum
+end
+
+def multiply (num1,num2)
+  num1*num2
+end
+
+def power (num1,num2)
+  num1**num2
+end
+
+def factorial (num1)
+  if num1 == 0
+    return 0
+  else
+    i = 1
+    factorialed = 1
+    until i > num1
+      factorialed *= i
+      i += 1
+    end
+    return factorialed
+  end
+end
