8dccbef4d1cd7abbe16a9ce0a1a3b055badfb99e
Anthony Bendas <anthony.bendas@gmail.com>
2017-10-13 21:45:40 -0600

    completed 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+
+def echo(str)
+  str
+end
+
+def shout(str)
+  str.upcase
+end
+
+def repeat(str, t = 2)
+  ([str]*t).join(" ")
+end
+
+def start_of_word(str, n)
+  str[0...n]
+end
+
+def first_word(str)
+  str.split.first
+end
+
+def titleize(str)
+  string = str.split.map do |word|
+    if %w(the and over).include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end
+  string.first.capitalize!
+  string.join(" ")
+end
